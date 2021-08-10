02a32e711446e6e01066294f34b5ad61232c0bac
JosephThomasPlappallil <jt287561@gmail.com>
2021-08-10 22:51:49 +0530

    first commit

---
+++ Input_string/Input_string/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            Console.WriteLine("Enter the Name : ");
+            Name=Console.ReadLine();
+            //Console.WriteLine("Hello "+Name);
+            Console.WriteLine("Name is : {0}",Name);
+        }
+    }
+}
---
+++ OOps/OOps/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOps
+{
+    class Data
+    {
+        public void Sravan()
+        {
+            Console.WriteLine("Hi i am Sravan");
+        }
+        internal void Shivam()
+        {
+            Console.WriteLine("Hi i am Shivam...");
+        
+        }
+        private void Lokesh()
+        {
+            Console.WriteLine("Hi i am Lokesh");
+        }
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data obj = new Data();
+            obj.Sravan();
+            obj.Shivam();
+
+        }
+    }
+}
